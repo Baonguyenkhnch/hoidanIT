24d342dee01cee4624394fd32b70ffbb0b6fe1aa
baonguyen <bao678393@gmail.com>
2025-10-04 14:55:03 +0700

    video46

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,13 +6,13 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>
   },
 ]);
-
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
